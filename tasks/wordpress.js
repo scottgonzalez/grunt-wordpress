539f5b1afd5f7902e830c5537917352001a55f5c
Scott González <scott.gonzalez@gmail.com>
2012-05-03 17:03:20 -0400

    Move taxonomies logic into a separate file. Move more logic into helpers.

--- tasks/wordpress.js
+++ tasks/wordpress.js
@@ -14,6 +14,7 @@ var _client,
 	fs = require( "fs" ),
 	path = require( "path" ),
 	wordpress = require( "wordpress" ),
+	taxonomies = require( grunt.task.getFile( "wordpress/taxonomies.js" ) )( grunt ),
 	async = grunt.utils.async;
 
 // Async directory recursion, always walks all files before recursing
@@ -30,216 +31,17 @@ function recurse( rootdir, fn, complete ) {
 	});
 }
 
-function getClient() {
-	if ( !_client ) {
-		_client = wordpress.createClient( grunt.config( "wordpress" ) );
-	}
-	return _client;
-}
-
 // Converts a postPath to a more readable name, e.g., "page/foo/bar" to "page foo/bar"
 function prettyName( postPath ) {
 	return postPath.replace( "/", " " );
 }
 
-// Converts a term to a readable name, e.g., { taxonomy: "foo", slug: "bar" } to "foo bar"
-function prettyTermName( term ) {
-	return term.taxonomy + " " + term.slug;
-}
-
-function getTaxonomies( fn ) {
-	var client = getClient();
-
-	async.waterfall([
-		function getAllTaxonomies( fn ) {
-			grunt.verbose.write( "Getting taxonomies from WordPress..." );
-			client.getTaxonomies( fn );
-		},
-
-		function getAllTerms( taxonomies, fn ) {
-			var all = {};
-			grunt.verbose.ok();
-			async.forEachSeries( taxonomies, function( taxonomy, fn ) {
-				all[ taxonomy.name ] = {};
-				grunt.verbose.write( "Getting " + taxonomy.name + " terms..." );
-				client.getTerms( taxonomy.name, function( error, terms ) {
-					var idMap = {};
-
-					if ( error ) {
-						grunt.verbose.error();
-						grunt.verbose.or.error( "Error getting " + taxonomy.name + "." );
-						return fn( error );
-					}
-
-					grunt.verbose.ok();
-
-					function expandSlug( term ) {
-						var slug = term.slug;
-						while ( term.parent !== "0" ) {
-							term = idMap[ term.parent ];
-							slug = term.slug + "/" + slug;
-						}
-						return slug;
-					}
-					terms.forEach(function( term ) {
-						idMap[ term.termId ] = term;
-					});
-
-					terms.forEach(function( term ) {
-						all[ taxonomy.name ][ expandSlug( term ) ] = term;
-					});
-					fn( null );
-				});
-			}, function( error ) {
-				if ( error ) {
-					return fn( error );
-				}
-
-				grunt.verbose.writeln();
-				fn( null, all );
-			});
-		}
-	], fn );
-}
-
-function createTerm( term, fn ) {
-	var client = getClient(),
-		name = prettyTermName( term );
-
-	if ( term.termId ) {
-		grunt.verbose.write( "Editing " + name + "..." );
-		client.editTerm( term.termId, term, function( error ) {
-			if ( error ) {
-				grunt.verbose.error();
-				return fn( error );
-			}
-
-			grunt.verbose.ok();
-			grunt.verbose.or.writeln( "Edited " + name + "." );
-			fn( null, term.termId );
-		});
-	} else {
-		grunt.verbose.write( "Creating " + name + "..." );
-		client.newTerm( term, function( error, termId ) {
-			if ( error ) {
-				grunt.verbose.error();
-				return fn( error );
-			}
-
-			grunt.verbose.ok();
-			grunt.verbose.or.writeln( "Created " + name + "." );
-			fn( null, termId );
-		});
+grunt.registerHelper( "wordpress-client", function() {
+	if ( !_client ) {
+		_client = wordpress.createClient( grunt.config( "wordpress" ) );
 	}
-}
-
-function processTaxonomies( path, fn ) {
-	var taxonomies,
-		client = getClient();
-
-	grunt.verbose.writeln( "Processing taxonomies.".bold );
-	try {
-		taxonomies = grunt.file.readJSON( path );
-	} catch( error ) {
-		grunt.log.error( "Invalid taxonomy definitions file." );
-		return fn( error );
-	}
-
-	async.waterfall([
-		getTaxonomies,
-
-		function publishTaxonomies( existingTaxonomies, fn ) {
-			async.forEachSeries( Object.keys( taxonomies ), function( taxonomy, fn ) {
-				// Taxonomies must already exist in WordPress
-				if ( !existingTaxonomies[ taxonomy ] ) {
-					grunt.log.error( "Taxonomies must exist in WordPress prior to use in taxonomies.json." );
-					return fn( new Error( "Invalid taxonomy: " + taxonomy ) );
-				}
-
-				function process( terms, parent, fn ) {
-					async.forEachSeries( terms, function( term, fn ) {
-						term.__slug = (parent ? parent.__slug + "/" : "") + term.slug;
-						if ( existingTaxonomies[ taxonomy ][ term.__slug ] ) {
-							term.termId = existingTaxonomies[ taxonomy ][ term.__slug ].termId;
-						}
-						term.taxonomy = taxonomy;
-						term.parent = parent ? parent.termId : null;
-						createTerm( term, function( error, termId ) {
-							if ( error ) {
-								grunt.verbose.or.error( "Error processing " + prettyTermName( term ) + "." );
-								return fn( error );
-							}
-
-							function done( error ) {
-								if ( error ) {
-									return fn( error );
-								}
-
-								delete existingTaxonomies[ taxonomy ][ term.__slug ];
-								fn( null, termId );
-							}
-
-							if ( !term.children ) {
-								return done();
-							}
-
-							// Process child terms
-							process( term.children, term, done );
-						});
-					}, function( error ) {
-						fn( error );
-					});
-				}
-
-				// Process top level terms
-				grunt.verbose.writeln( "Processing terms.".bold );
-				process( taxonomies[ taxonomy ], null, fn );
-			}, function( error ) {
-				if ( error ) {
-					return fn( error );
-				}
-
-				grunt.verbose.writeln();
-				fn( null, existingTaxonomies );
-			});
-		},
-
-		// TODO: Don't delete taxonomies until after processing posts.
-		// This will allow us to use keywords without defining all of them upfront.
-		function deleteTaxonomies( taxonomies, fn ) {
-			grunt.verbose.writeln( "Deleting old terms.".bold );
-			async.map( Object.keys( taxonomies ), function( taxonomyName, fn ) {
-				var taxonomy = taxonomies[ taxonomyName ];
-				async.forEachSeries( Object.keys( taxonomy ), function( term, fn ) {
-					var name;
-					term = taxonomy[ term ];
-					name = prettyTermName( term );
-					grunt.verbose.write( "Deleting " + name + "..." );
-					client.deleteTerm( taxonomyName, term.termId, function( error ) {
-						if ( error ) {
-							grunt.verbose.error();
-							grunt.verbose.or.error( "Error deleting " + name + "." );
-							return fn( error );
-						}
-
-						grunt.verbose.ok();
-						grunt.verbose.or.writeln( "Deleted " + name + "." );
-						fn( null );
-					});
-				}, fn );
-			}, function( error ) {
-				if ( error ) {
-					return fn( error );
-				}
-
-				grunt.verbose.writeln();
-				fn( null );
-			});
-		}
-	], function( error ) {
-		fn( error );
-	});
-}
+	return _client;
+});
 
 // TODO: Smarter updates (compare checksums and only republish if there were changes)
 grunt.registerTask( "wordpress-publish", "Generate posts in WordPress from HTML files", function() {
@@ -247,10 +49,10 @@ grunt.registerTask( "wordpress-publish", "Generate posts in WordPress from HTML 
 	var done = this.async(),
 		dir = grunt.config( "wordpress.dir" );
 	async.waterfall([
-		function taxonomies( fn ) {
+		function processTaxonomies( fn ) {
 			var taxonomiesPath = path.join( dir, "taxonomies.json" );
 			if ( path.existsSync( taxonomiesPath ) ) {
-				processTaxonomies( taxonomiesPath, fn );
+				taxonomies.process( taxonomiesPath, fn );
 			} else {
 				fn( null );
 			}
@@ -258,7 +60,7 @@ grunt.registerTask( "wordpress-publish", "Generate posts in WordPress from HTML 
 
 		function getPostPaths( fn ) {
 			grunt.verbose.write( "Getting post paths from WordPress..." );
-			getClient().call( "gw.getPostPaths", "any", fn );
+			grunt.helper( "wordpress-client" ).call( "gw.getPostPaths", "any", fn );
 		},
 
 		function publishPosts( postPaths, fn ) {
@@ -297,7 +99,7 @@ grunt.registerTask( "wordpress-publish", "Generate posts in WordPress from HTML 
 		},
 
 		function deletePosts( postPaths, fn ) {
-			var client = getClient();
+			var client = grunt.helper( "wordpress-client" );
 
 			grunt.verbose.writeln( "Deleting old posts.".bold );
 			async.map( Object.keys( postPaths ), function( postPath, fn ) {
@@ -364,7 +166,8 @@ grunt.registerTask( "wordpress-validate", "Validate HTML files for publishing to
 	// - Verify that jQuery Slugs plugin exists
 	// - Verify taxonomies.js
 	//    - Requires name, slug
-	//    - slug must be [a-z0-9.-]
+	//    - Slug must be [a-z0-9.-], no consecutive dashes
+	//    - Check for existing terms with same name, but different slug
 
 	grunt.helper( "wordpress-walk-posts", dir, function( post, fn ) {
 		count++;
@@ -428,7 +231,7 @@ grunt.registerHelper( "wordpress-parse-post", function( path ) {
 
 // Publish (create or update) a post to WordPress.
 grunt.registerHelper( "wordpress-publish-post", function( post, fn ) {
-	var client = getClient(),
+	var client = grunt.helper( "wordpress-client" ),
 		name = prettyName( post.__postPath );
 
 	if ( post.id ) {
---
+++ tasks/wordpress/taxonomies.js
@@ -0,0 +1,213 @@
+module.exports = function( grunt ) {
+
+var async = grunt.utils.async;
+
+// Converts a term to a readable name, e.g., { taxonomy: "foo", slug: "bar" } to "foo bar"
+function prettyTermName( term ) {
+	return term.taxonomy + " " + term.slug;
+}
+
+grunt.registerHelper( "wordpress-get-terms", function( fn ) {
+	var client = grunt.helper( "wordpress-client" );
+
+	async.waterfall([
+		function getAllTaxonomies( fn ) {
+			grunt.verbose.write( "Getting taxonomies from WordPress..." );
+			client.getTaxonomies( fn );
+		},
+
+		function getAllTerms( taxonomies, fn ) {
+			var all = {};
+			grunt.verbose.ok();
+			async.forEachSeries( taxonomies, function( taxonomy, fn ) {
+				all[ taxonomy.name ] = {};
+				grunt.verbose.write( "Getting " + taxonomy.name + " terms..." );
+				client.getTerms( taxonomy.name, function( error, terms ) {
+					var idMap = {};
+
+					if ( error ) {
+						grunt.verbose.error();
+						grunt.verbose.or.error( "Error getting " + taxonomy.name + "." );
+						return fn( error );
+					}
+
+					grunt.verbose.ok();
+
+					function expandSlug( term ) {
+						var slug = term.slug;
+						while ( term.parent !== "0" ) {
+							term = idMap[ term.parent ];
+							slug = term.slug + "/" + slug;
+						}
+						return slug;
+					}
+					terms.forEach(function( term ) {
+						idMap[ term.termId ] = term;
+					});
+
+					terms.forEach(function( term ) {
+						all[ taxonomy.name ][ expandSlug( term ) ] = term;
+					});
+					fn( null );
+				});
+			}, function( error ) {
+				if ( error ) {
+					return fn( error );
+				}
+
+				grunt.verbose.writeln();
+				fn( null, all );
+			});
+		}
+	], fn );
+});
+
+grunt.registerHelper( "wordpress-publish-term", function( term, fn ) {
+	var client = grunt.helper( "wordpress-client" ),
+		name = prettyTermName( term );
+
+	if ( term.termId ) {
+		grunt.verbose.write( "Editing " + name + "..." );
+		client.editTerm( term.termId, term, function( error ) {
+			if ( error ) {
+				grunt.verbose.error();
+				return fn( error );
+			}
+
+			grunt.verbose.ok();
+			grunt.verbose.or.writeln( "Edited " + name + "." );
+			fn( null, term.termId );
+		});
+	} else {
+		grunt.verbose.write( "Creating " + name + "..." );
+		client.newTerm( term, function( error, termId ) {
+			if ( error ) {
+				grunt.verbose.error();
+				return fn( error );
+			}
+
+			grunt.verbose.ok();
+			grunt.verbose.or.writeln( "Created " + name + "." );
+			fn( null, termId );
+		});
+	}
+});
+
+grunt.registerHelper( "wordpress-delete-term", function( term, fn ) {
+	var client = grunt.helper( "wordpress-client" ),
+		name = prettyTermName( term );
+	grunt.verbose.write( "Deleting " + name + "..." );
+	client.deleteTerm( term.taxonomy, term.termId, function( error ) {
+		if ( error ) {
+			grunt.verbose.error();
+			grunt.verbose.or.error( "Error deleting " + name + "." );
+			return fn( error );
+		}
+
+		grunt.verbose.ok();
+		grunt.verbose.or.writeln( "Deleted " + name + "." );
+		fn( null );
+	});
+});
+
+function processTaxonomies( path, fn ) {
+	var taxonomies,
+		client = grunt.helper( "wordpress-client" );
+
+	grunt.verbose.writeln( "Processing taxonomies.".bold );
+	try {
+		taxonomies = grunt.file.readJSON( path );
+	} catch( error ) {
+		grunt.log.error( "Invalid taxonomy definitions file." );
+		return fn( error );
+	}
+
+	async.waterfall([
+		function getTaxonomies( fn ) {
+			grunt.helper( "wordpress-get-terms", fn );
+		},
+
+		function publishTaxonomies( existingTaxonomies, fn ) {
+			async.forEachSeries( Object.keys( taxonomies ), function( taxonomy, fn ) {
+				// Taxonomies must already exist in WordPress
+				if ( !existingTaxonomies[ taxonomy ] ) {
+					grunt.log.error( "Taxonomies must exist in WordPress prior to use in taxonomies.json." );
+					return fn( new Error( "Invalid taxonomy: " + taxonomy ) );
+				}
+
+				function process( terms, parent, fn ) {
+					async.forEachSeries( terms, function( term, fn ) {
+						term.__slug = (parent ? parent.__slug + "/" : "") + term.slug;
+						if ( existingTaxonomies[ taxonomy ][ term.__slug ] ) {
+							term.termId = existingTaxonomies[ taxonomy ][ term.__slug ].termId;
+						}
+						term.taxonomy = taxonomy;
+						term.parent = parent ? parent.termId : null;
+						grunt.helper( "wordpress-publish-term", term, function( error, termId ) {
+							if ( error ) {
+								grunt.verbose.or.error( "Error processing " + prettyTermName( term ) + "." );
+								return fn( error );
+							}
+
+							function done( error ) {
+								if ( error ) {
+									return fn( error );
+								}
+
+								delete existingTaxonomies[ taxonomy ][ term.__slug ];
+								fn( null, termId );
+							}
+
+							if ( !term.children ) {
+								return done();
+							}
+
+							// Process child terms
+							process( term.children, term, done );
+						});
+					}, function( error ) {
+						fn( error );
+					});
+				}
+
+				// Process top level terms
+				grunt.verbose.writeln( "Processing terms.".bold );
+				process( taxonomies[ taxonomy ], null, fn );
+			}, function( error ) {
+				if ( error ) {
+					return fn( error );
+				}
+
+				grunt.verbose.writeln();
+				fn( null, existingTaxonomies );
+			});
+		},
+
+		// TODO: Don't delete taxonomies until after processing posts.
+		// This will allow us to use keywords without defining all of them upfront.
+		function deleteTaxonomies( taxonomies, fn ) {
+			grunt.verbose.writeln( "Deleting old terms.".bold );
+			async.map( Object.keys( taxonomies ), function( taxonomyName, fn ) {
+				var taxonomy = taxonomies[ taxonomyName ];
+				async.forEachSeries( Object.keys( taxonomy ), function( term, fn ) {
+					grunt.helper( "wordpress-delete-term", taxonomy[ term ], fn );
+				}, fn );
+			}, function( error ) {
+				if ( error ) {
+					return fn( error );
+				}
+
+				grunt.verbose.writeln();
+				fn( null );
+			});
+		}
+	], function( error ) {
+		fn( error );
+	});
+}
+
+return {
+	process: processTaxonomies
+};
+
+};
