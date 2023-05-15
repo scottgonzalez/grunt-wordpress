1fc82683284aa931eb6c88d4dc3b8f78deea0642
Scott González <scott.gonzalez@gmail.com>
2023-05-15 15:17:48 -0400

    Fix preversion script

--- scripts/preversion.js
+++ scripts/preversion.js
@@ -2,8 +2,9 @@
 
 const fs = require('fs')
 const path = require('path')
+const package = require('../package');
 
-const definedVersion = process.env.npm_package_dependencies_gilded_wordpress
+const definedVersion = package.dependencies['gilded-wordpress'];
 const gildedPath = require.resolve('gilded-wordpress/gilded-wordpress.php')
 const gruntPath = path.join(__dirname, '../gilded-wordpress.php')
 const gildedSource = fs.readFileSync(gildedPath, 'utf8')
